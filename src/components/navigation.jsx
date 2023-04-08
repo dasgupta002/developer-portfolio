3bc9ea5c731010826771b918c13e1383cfecc6ef
Subhrakanti Dasgupta <subhrakanti.dasgupta@gmail.com>
2023-04-08 15:21:05 +0530

    Removed junk.

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -7,12 +7,12 @@ export default function Navbar() {
         <nav className = 'flex flex-wrap justify-between font-extrabold'>
             { location.pathname !== '/' && <NavLink to = '/' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>home</NavLink> }
             <NavLink to = '/about' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>about</NavLink>
-            <NavLink to = 'https://www.linkedin.com/in/dasgupta002/' target = '_blank' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>hire me</NavLink>
+            <NavLink to = 'https://www.linkedin.com/in/dasgupta002/' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>hire me</NavLink>
             <NavLink to = '/projects' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>projects</NavLink>
             <NavLink to = '/posts' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>posts</NavLink>
-            <NavLink to = 'https://www.linkedin.com/in/dasgupta002/' target = '_blank' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>talks</NavLink>
-            <NavLink to = './assets/resume.pdf' target = '_blank' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>resume</NavLink>
-            <NavLink to = 'https://calcuttahighways.netlify.app/' target = '_blank' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>blog</NavLink>
+            <NavLink to = 'https://www.linkedin.com/in/dasgupta002/' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>talks</NavLink>
+            <NavLink to = './assets/resume.pdf' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>resume</NavLink>
+            <NavLink to = 'https://calcuttahighways.netlify.app/' className = 'hover:bg-black hover:text-white rounded px-4 py-2'>blog</NavLink>
         </nav>
     )
 }
